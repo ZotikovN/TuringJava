f0f04cae7b29bdc2a201cad185f315145adaf1ac
ZotikovN <wwwkooly12@gmail.com>
2019-06-30 20:54:35 +0300

    Обозначил комманды

---
+++ src/Calculation.java
@@ -0,0 +1,24 @@
+
+import javax.swing.plaf.nimbus.State;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+
+public class Calculation {
+    private boolean start;
+    private String commands;
+    private String output;
+
+
+    Calculation(boolean start, String commands, String output){
+        this.start = start;
+        this.commands = commands;
+        this.output = output;
+    }
+
+    void start(){
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.util.*;
+
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().launch(args);
+    }
+
+    private void launch(String[] args) {
+        CmdLineParser cmd = new CmdLineParser(this);
+        try {
+            cmd.parseArgument(args);
+        } catch (CmdLineException a) {
+            System.err.println(a.getMessage());
+            cmd.printUsage(System.err);
+        }
+        if (commands.isEmpty())
+            throw new IllegalArgumentException("Нет файла с коммандами");
+        if (output.isEmpty())
+            throw new IllegalArgumentException("Нет выходного файла");
+
+        Calculation main = new Calculation(start, commands, output);
+        main.start();
+    }
+
+    //Комманды ввода в консоль
+
+    @Option(name = "-start", usage = "start")
+    private boolean start = false;
+
+    @Option(name = "-commands", usage = "commands")
+    private String commands = "";
+
+    @Option(name = "-output", usage = "output")
+    private String output = "";
+}
