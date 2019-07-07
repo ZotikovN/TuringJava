4991bf3bf2ee951eec18e9671122bb928a6cdb00
ZotikovN <wwwkooly12@gmail.com>
2019-07-07 19:15:31 +0300

    реализовал класс ленты

--- src/Tape.java
+++ src/Tape.java
@@ -1,12 +1,39 @@
 import java.util.ArrayList;
 
+//Класс ленты
+
 public class Tape {
     ArrayList<Character> charIn = new ArrayList<>();
     int state;
 
-    Tape (int start){
+    Tape (int start, String chars){
         this.state = start;
+        for (int i = 0; i < chars.length(); i++) {
+            charIn.add(chars.charAt(i));
+        }
+    }
+
+
+    void goForward(boolean i){
+        if (i) {
+            state++;
+        }
+        else {
+            if (state != 0)
+            {
+                state--;
+            }
+            else {
+                throw new IllegalArgumentException();
+            }
+        }
 
     }
+
+    public int getState() {
+        return state;
+    }
+
+
 
 }
